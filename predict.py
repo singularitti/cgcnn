d9b423cbac71e7eed47a80aa2a8ec95a13bb8e5a
singularitti <singularitti@outlook.com>
2025-11-19 13:33:56 -0600

    Deprecate main.py & predict.py

--- predict.py
+++
@@ -1,8 +0,0 @@
-"""Compatibility wrapper that exposes the `predict_model` function from the
-`cgcnn` package. The CLI is removed and can be used from Python REPL or scripts
-by importing `cgcnn.inference.predict_model`.
-"""
-
-from cgcnn.inference import predict_model
-
-__all__ = ["predict_model"]
