dba6da6015fa18a0a4373410a3fb19469f021baf
singularitti <singularitti@outlook.com>
2025-12-14 00:54:02 -0600

    Reformat src/cgcnn/data.py

--- src/cgcnn/data.py
+++ src/cgcnn/data.py
@@ -1,5 +1,3 @@
-from __future__ import print_function, division
-
 import csv
 import functools
 import json
@@ -24,10 +22,18 @@ __all__ = [
 ]
 
 
-def get_train_val_test_loader(dataset, collate_fn=default_collate,
-                              batch_size=64, train_ratio=None,
-                              val_ratio=0.1, test_ratio=0.1, return_test=False,
-                              num_workers=1, pin_memory=False, **kwargs):
+def get_train_val_test_loader(
+    dataset,
+    collate_fn=default_collate,
+    batch_size=64,
+    train_ratio=None,
+    val_ratio=0.1,
+    test_ratio=0.1,
+    return_test=False,
+    num_workers=1,
+    pin_memory=False,
+    **kwargs,
+):
     """
     Utility function for dividing a dataset to train, val, test datasets.
 
@@ -59,45 +65,58 @@ def get_train_val_test_loader(dataset, collate_fn=default_collate,
         return_test=True.
     """
     total_size = len(dataset)
-    if kwargs['train_size'] is None:
+    if kwargs["train_size"] is None:
         if train_ratio is None:
             assert val_ratio + test_ratio < 1
             train_ratio = 1 - val_ratio - test_ratio
-            print(f'[Warning] train_ratio is None, using 1 - val_ratio - '
-                  f'test_ratio = {train_ratio} as training data.')
+            print(
+                f"[Warning] train_ratio is None, using 1 - val_ratio - "
+                f"test_ratio = {train_ratio} as training data."
+            )
         else:
             assert train_ratio + val_ratio + test_ratio <= 1
     indices = list(range(total_size))
-    if kwargs['train_size']:
-        train_size = kwargs['train_size']
+    if kwargs["train_size"]:
+        train_size = kwargs["train_size"]
     else:
         train_size = int(train_ratio * total_size)
-    if kwargs['test_size']:
-        test_size = kwargs['test_size']
+    if kwargs["test_size"]:
+        test_size = kwargs["test_size"]
     else:
         test_size = int(test_ratio * total_size)
-    if kwargs['val_size']:
-        valid_size = kwargs['val_size']
+    if kwargs["val_size"]:
+        valid_size = kwargs["val_size"]
     else:
         valid_size = int(val_ratio * total_size)
     train_sampler = SubsetRandomSampler(indices[:train_size])
-    val_sampler = SubsetRandomSampler(
-        indices[-(valid_size + test_size):-test_size])
+    val_sampler = SubsetRandomSampler(indices[-(valid_size + test_size) : -test_size])
     if return_test:
         test_sampler = SubsetRandomSampler(indices[-test_size:])
-    train_loader = DataLoader(dataset, batch_size=batch_size,
-                              sampler=train_sampler,
-                              num_workers=num_workers,
-                              collate_fn=collate_fn, pin_memory=pin_memory)
-    val_loader = DataLoader(dataset, batch_size=batch_size,
-                            sampler=val_sampler,
-                            num_workers=num_workers,
-                            collate_fn=collate_fn, pin_memory=pin_memory)
+    train_loader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        sampler=train_sampler,
+        num_workers=num_workers,
+        collate_fn=collate_fn,
+        pin_memory=pin_memory,
+    )
+    val_loader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        sampler=val_sampler,
+        num_workers=num_workers,
+        collate_fn=collate_fn,
+        pin_memory=pin_memory,
+    )
     if return_test:
-        test_loader = DataLoader(dataset, batch_size=batch_size,
-                                 sampler=test_sampler,
-                                 num_workers=num_workers,
-                                 collate_fn=collate_fn, pin_memory=pin_memory)
+        test_loader = DataLoader(
+            dataset,
+            batch_size=batch_size,
+            sampler=test_sampler,
+            num_workers=num_workers,
+            collate_fn=collate_fn,
+            pin_memory=pin_memory,
+        )
     if return_test:
         return train_loader, val_loader, test_loader
     else:
@@ -141,23 +160,28 @@ def collate_pool(dataset_list):
     crystal_atom_idx, batch_target = [], []
     batch_cif_ids = []
     base_idx = 0
-    for i, ((atom_fea, nbr_fea, nbr_fea_idx), target, cif_id)\
-            in enumerate(dataset_list):
+    for i, ((atom_fea, nbr_fea, nbr_fea_idx), target, cif_id) in enumerate(
+        dataset_list
+    ):
         n_i = atom_fea.shape[0]  # number of atoms for this crystal
         batch_atom_fea.append(atom_fea)
         batch_nbr_fea.append(nbr_fea)
-        batch_nbr_fea_idx.append(nbr_fea_idx+base_idx)
-        new_idx = torch.LongTensor(np.arange(n_i)+base_idx)
+        batch_nbr_fea_idx.append(nbr_fea_idx + base_idx)
+        new_idx = torch.LongTensor(np.arange(n_i) + base_idx)
         crystal_atom_idx.append(new_idx)
         batch_target.append(target)
         batch_cif_ids.append(cif_id)
         base_idx += n_i
-    return (torch.cat(batch_atom_fea, dim=0),
+    return (
+        (
+            torch.cat(batch_atom_fea, dim=0),
             torch.cat(batch_nbr_fea, dim=0),
             torch.cat(batch_nbr_fea_idx, dim=0),
-            crystal_atom_idx),\
-        torch.stack(batch_target, dim=0),\
-        batch_cif_ids
+            crystal_atom_idx,
+        ),
+        torch.stack(batch_target, dim=0),
+        batch_cif_ids,
+    )
 
 
 class GaussianDistance(object):
@@ -166,6 +190,7 @@ class GaussianDistance(object):
 
     Unit: angstrom
     """
+
     def __init__(self, dmin, dmax, step, var=None):
         """
         Parameters
@@ -180,7 +205,7 @@ class GaussianDistance(object):
         """
         assert dmin < dmax
         assert dmax - dmin > step
-        self.filter = np.arange(dmin, dmax+step, step)
+        self.filter = np.arange(dmin, dmax + step, step)
         if var is None:
             var = step
         self.var = var
@@ -201,8 +226,7 @@ class GaussianDistance(object):
           Expanded distance matrix with the last dimension of length
           len(self.filter)
         """
-        return np.exp(-(distances[..., np.newaxis] - self.filter)**2 /
-                      self.var**2)
+        return np.exp(-((distances[..., np.newaxis] - self.filter) ** 2) / self.var**2)
 
 
 class AtomInitializer(object):
@@ -211,6 +235,7 @@ class AtomInitializer(object):
 
     !!! Use one AtomInitializer per dataset !!!
     """
+
     def __init__(self, atom_types):
         self.atom_types = set(atom_types)
         self._embedding = {}
@@ -222,16 +247,18 @@ class AtomInitializer(object):
     def load_state_dict(self, state_dict):
         self._embedding = state_dict
         self.atom_types = set(self._embedding.keys())
-        self._decodedict = {idx: atom_type for atom_type, idx in
-                            self._embedding.items()}
+        self._decodedict = {
+            idx: atom_type for atom_type, idx in self._embedding.items()
+        }
 
     def state_dict(self):
         return self._embedding
 
     def decode(self, idx):
-        if not hasattr(self, '_decodedict'):
-            self._decodedict = {idx: atom_type for atom_type, idx in
-                                self._embedding.items()}
+        if not hasattr(self, "_decodedict"):
+            self._decodedict = {
+                idx: atom_type for atom_type, idx in self._embedding.items()
+            }
         return self._decodedict[idx]
 
 
@@ -247,11 +274,11 @@ class AtomCustomJSONInitializer(AtomInitializer):
     elem_embedding_file: str
         The path to the .json file
     """
+
     def __init__(self, elem_embedding_file):
         with open(elem_embedding_file) as f:
             elem_embedding = json.load(f)
-        elem_embedding = {int(key): value for key, value
-                          in elem_embedding.items()}
+        elem_embedding = {int(key): value for key, value in elem_embedding.items()}
         atom_types = set(elem_embedding.keys())
         super(AtomCustomJSONInitializer, self).__init__(atom_types)
         for key, value in elem_embedding.items():
@@ -307,32 +334,35 @@ class CIFData(Dataset):
     target: torch.Tensor shape (n_targets, )
     cif_id: str or int
     """
-    def __init__(self, root_dir, max_num_nbr=12, radius=8, dmin=0, step=0.2,
-                 random_seed=123):
+
+    def __init__(
+        self, root_dir, max_num_nbr=12, radius=8, dmin=0, step=0.2, random_seed=123
+    ):
         self.root_dir = root_dir
         self.max_num_nbr, self.radius = max_num_nbr, radius
-        assert os.path.exists(root_dir), 'root_dir does not exist!'
-        id_prop_file = os.path.join(self.root_dir, 'id_prop.csv')
-        assert os.path.exists(id_prop_file), 'id_prop.csv does not exist!'
+        assert os.path.exists(root_dir), "root_dir does not exist!"
+        id_prop_file = os.path.join(self.root_dir, "id_prop.csv")
+        assert os.path.exists(id_prop_file), "id_prop.csv does not exist!"
         with open(id_prop_file) as f:
             reader = csv.reader(f)
             self.id_prop_data = [row for row in reader if row]
         if not self.id_prop_data:
-            raise ValueError('id_prop.csv is empty!')
+            raise ValueError("id_prop.csv is empty!")
         self.n_targets = len(self.id_prop_data[0]) - 1
         if self.n_targets < 1:
-            raise ValueError('id_prop.csv must contain at least one target column.')
+            raise ValueError("id_prop.csv must contain at least one target column.")
         for row in self.id_prop_data:
             if len(row) != self.n_targets + 1:
                 raise ValueError(
-                    'All rows in id_prop.csv must have the same number of columns. '
-                    'Expected {} target columns but got {} for id {}.'.format(
-                        self.n_targets, len(row) - 1,
-                        row[0] if row else 'unknown'))
+                    "All rows in id_prop.csv must have the same number of columns. "
+                    "Expected {} target columns but got {} for id {}.".format(
+                        self.n_targets, len(row) - 1, row[0] if row else "unknown"
+                    )
+                )
         random.seed(random_seed)
         random.shuffle(self.id_prop_data)
-        atom_init_file = os.path.join(self.root_dir, 'atom_init.json')
-        assert os.path.exists(atom_init_file), 'atom_init.json does not exist!'
+        atom_init_file = os.path.join(self.root_dir, "atom_init.json")
+        assert os.path.exists(atom_init_file), "atom_init.json does not exist!"
         self.ari = AtomCustomJSONInitializer(atom_init_file)
         self.gdf = GaussianDistance(dmin=dmin, dmax=self.radius, step=step)
 
@@ -343,29 +373,31 @@ class CIFData(Dataset):
     def __getitem__(self, idx):
         row = self.id_prop_data[idx]
         cif_id, target_values = row[0], row[1:]
-        crystal = Structure.from_file(os.path.join(self.root_dir,
-                                                   cif_id+'.cif'))
-        atom_fea = np.vstack([self.ari.get_atom_fea(crystal[i].specie.number)
-                              for i in range(len(crystal))])
+        crystal = Structure.from_file(os.path.join(self.root_dir, cif_id + ".cif"))
+        atom_fea = np.vstack([
+            self.ari.get_atom_fea(crystal[i].specie.number) for i in range(len(crystal))
+        ])
         atom_fea = torch.Tensor(atom_fea)
         all_nbrs = crystal.get_all_neighbors(self.radius, include_index=True)
         all_nbrs = [sorted(nbrs, key=lambda x: x[1]) for nbrs in all_nbrs]
         nbr_fea_idx, nbr_fea = [], []
         for nbr in all_nbrs:
             if len(nbr) < self.max_num_nbr:
-                warnings.warn('{} not find enough neighbors to build graph. '
-                              'If it happens frequently, consider increase '
-                              'radius.'.format(cif_id))
-                nbr_fea_idx.append(list(map(lambda x: x[2], nbr)) +
-                                   [0] * (self.max_num_nbr - len(nbr)))
-                nbr_fea.append(list(map(lambda x: x[1], nbr)) +
-                               [self.radius + 1.] * (self.max_num_nbr -
-                                                     len(nbr)))
+                warnings.warn(
+                    "{} not find enough neighbors to build graph. "
+                    "If it happens frequently, consider increase "
+                    "radius.".format(cif_id)
+                )
+                nbr_fea_idx.append(
+                    list(map(lambda x: x[2], nbr)) + [0] * (self.max_num_nbr - len(nbr))
+                )
+                nbr_fea.append(
+                    list(map(lambda x: x[1], nbr))
+                    + [self.radius + 1.0] * (self.max_num_nbr - len(nbr))
+                )
             else:
-                nbr_fea_idx.append(list(map(lambda x: x[2],
-                                            nbr[:self.max_num_nbr])))
-                nbr_fea.append(list(map(lambda x: x[1],
-                                        nbr[:self.max_num_nbr])))
+                nbr_fea_idx.append(list(map(lambda x: x[2], nbr[: self.max_num_nbr])))
+                nbr_fea.append(list(map(lambda x: x[1], nbr[: self.max_num_nbr])))
         nbr_fea_idx, nbr_fea = np.array(nbr_fea_idx), np.array(nbr_fea)
         nbr_fea = self.gdf.expand(nbr_fea)
         atom_fea = torch.Tensor(atom_fea)  # This line is useless
